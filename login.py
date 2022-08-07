e06e5e0de1c012be44df562168efd1c44c7bcc32
matheusAparicio <matheusilva-m@outlook.com>
2022-08-07 14:00:31 -0300

    login class fixed & code cleaning

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Login:
 
     def __init__(self):
@@ -8,34 +9,37 @@ class Login:
         self.verifyLoginFile()
 
     def verifyLoginFile(self):
-        if (os.path.exists("credentials.txt")):
+        if os.path.exists("credentials.txt"):
             print("Já existem credenciais de login. Usá-las para os teste?")
             loginChoice = input("(S/s): Você usará as credenciais salvas para logar.\n"
                                 "(N/n): As credenciais atuais serão apagadas e você digitará outra.\n"
                                 "Qual sua escolha?: ")
-            if (loginChoice.lower() == 's'):
-                pass
+            if loginChoice.lower() == 's':
+                with open("credentials.txt", 'r') as file:
+                    self.userEmail = file.readline()[:-1]
+                    self.userPassword = file.readline()
             else:
                 self.deleteLoginInfo()
                 self.getLoginInfo()
         else:
             self.getLoginInfo()
 
-
     def getLoginInfo(self):
         self.userEmail = input("Digite seu email para login no Github: ")
         self.userPassword = input("Digite sua senha: ")
         persistChoice = input("Gostaria de salvar suas credenciais?"
                               "(s/n): ")
-        if (persistChoice.lower() == 's'):
+        if persistChoice.lower() == 's':
             self.persistLoginInfo()
         else:
             pass
 
     def persistLoginInfo(self):
-        file = open("credentials.txt", 'a')
-        file.write(f"{self.userEmail}\n{self.userPassword}")
+        file = open("credentials.txt", 'w')
+        file.write(f"{self.userEmail}\n"
+                   f"{self.userPassword}")
         file.close()
 
-    def deleteLoginInfo(self):
+    @staticmethod
+    def deleteLoginInfo():
         os.remove("credentials.txt")
