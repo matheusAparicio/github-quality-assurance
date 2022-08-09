83dd7efa4241c936bad7f30744dbba8870a961a0
matheusAparicio <matheusilva-m@outlook.com>
2022-08-09 03:33:45 -0300

    search validator code improved & comments

--- validators/search_validator.py
+++ validators/search_validator.py
@@ -9,15 +9,21 @@ class SearchValidator(GithubValidator):
 
     def __init__(self, driver: webdriver):
         super().__init__(driver)
+        # Words with almost granted search results.
         self.commonWords = ["project", 'a', "car", "light", "color", "api", "ufsc", "letter", "matheus"]
 
     def validateSearch(self):
         testsPassed = 0
+        failedWords = []
         for word in self.commonWords:
             if self.validateSingleSearch(word):
                 testsPassed += 1
+            else:
+                failedWords.append(word)
+
         if testsPassed < len(self.commonWords):
-            print("[!] Search failed!")
+            print(f"[!] Search failed! {testsPassed}/{len(self.commonWords)} tests passed. "
+                  f"The failed words was {failedWords}")
         else:
             print("[+] Search successful!")
 
@@ -28,6 +34,13 @@ class SearchValidator(GithubValidator):
         searchBar.send_keys(searchTerm)
         searchBar.submit()
 
+        # If both tests are successfully, the search is validated.
+        if self.__validateSingleSearchMatchNumber(searchTerm) and self.__validateSingleSearchMatches(searchTerm):
+            return True
+        return False
+
+    # Check if more than 0 results exists in at least half of the search categories verified.
+    def __validateSingleSearchMatchNumber(self, searchTerm: str):
         # Check if at last 2 search match counter are visible.
         WebDriverWait(self.driver, timeout=10).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, "span.js-codesearch-count")),
@@ -43,6 +56,20 @@ class SearchValidator(GithubValidator):
                 if i.text == '0':
                     zeroMatchesFound += 1
 
+        # If at least half of the matches counters has a value different from 0.
+        if zeroMatchesFound < len(counterTexts) / 2:
+            return True
+        return False
+
+    # Get the search results and pass through repositories names and descriptions.
+    # If the searchTerm is encountered in at least half of these, the search term is validated.
+    def __validateSingleSearchMatches(self, searchTerm: str):
+        # Check if at last 2 possible search match are visible.
+        WebDriverWait(self.driver, timeout=10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "v-align-middle")),
+            EC.visibility_of_element_located((By.CLASS_NAME, "mb-1"))
+        )
+
         resultTitles = []
         for i in self.driver.find_elements(By.CLASS_NAME, "v-align-middle"):
             if i.is_displayed() and i.text != '':
@@ -55,8 +82,7 @@ class SearchValidator(GithubValidator):
             if searchTerm.lower() in i.text.lower():
                 searchMatches += 1
 
-        # If at least half of the matches counters has a value different from 0 and
-        # if search term match with enough repository titles & descriptions.
-        if zeroMatchesFound < len(counterTexts) / 2 and searchMatches >= (len(loadedResults) // 2):
+        # If search term match with enough repository titles & descriptions.
+        if searchMatches >= (len(loadedResults) // 2):
             return True
         return False
